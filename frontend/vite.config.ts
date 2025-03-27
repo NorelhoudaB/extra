d4ac0e75dd98563dfe675c3e349c4e70942cef1c
NorelhoudaB <norelhouda.brioua@univ-constantine2.dz>
2025-03-27 09:54:01 +0100

    - fixed docker port frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,13 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    },
-    port: 8911
+    host: '0.0.0.0', 
+    port: 8911, 
   }
 })
